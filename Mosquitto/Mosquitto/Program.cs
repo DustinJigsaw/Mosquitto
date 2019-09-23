a810ec5eee4f43c9dde17cf32f4ad09ece9c2642
DustinJigsaw <dustingutierrez99@gmail.com>
2019-09-23 16:39:54 -0300

    Mosquitto

---
+++ Mosquitto/Mosquitto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Mqtt;
+using System.Threading.Tasks;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace daenet.iot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine("Hello, World!");
+
+            var mqttClient = MqttClient.CreateAsync("localhost").Result;
+
+            var sess = mqttClient.ConnectAsync().Result;
+
+            string rcvTopic = "et12/lab22";
+            string sendTopic = "et12/lab22";
+
+            mqttClient.SubscribeAsync(rcvTopic, MqttQualityOfService.ExactlyOnce);
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+                    Console.WriteLine("Enter the text to send.");
+
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+
+                    var line = System.Console.ReadLine();
+
+                    var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(line));
+
+                    mqttClient.PublishAsync(new MqttApplicationMessage(sendTopic, data), MqttQualityOfService.ExactlyOnce).Wait();
+                }
+            });
+
+            mqttClient.MessageStream.Subscribe(msg =>
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine(Encoding.UTF8.GetString(msg.Payload));
+
+                Console.ResetColor();
+            });
+        }
+    }
+}
